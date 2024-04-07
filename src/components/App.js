e0f5e55f754f8cb0240463e77df66974454d9b96
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-04-07 15:16:30 +0530

    impl

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useMemo, useState } from "react";
+import "./../styles/App.css";
+import "regenerator-runtime/runtime";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [data, setData] = useState([]);
 
-export default App
+  async function fetchData() {
+    try {
+      let res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      let result = await res.json();
+      console.log(result);
+      setData(result ?? []);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const catchedValue = useMemo(() => {
+    return data;
+  }, [data]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div className="app">
+      <ul>
+        {catchedValue.length > 0 ? (
+          catchedValue?.map((val, i) => {
+            return (
+              <div key={i}>
+                <li>{val.title ?? ""}</li>
+                <p>{val.body ?? ""}</p>
+              </div>
+            );
+          })
+        ) : (
+          <h2>Loading...</h2>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
